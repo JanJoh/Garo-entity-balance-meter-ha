a9d432e9b99c232d8f30066473d023fda287fdf1
JanJoh <j2@mupp.net>
2025-07-15 06:38:20 +0200

    Update __init__.py
    Fixed deprecated syntax for init

--- custom_components/garo_entity_balance_meter/__init__.py
+++ custom_components/garo_entity_balance_meter/__init__.py
@@ -4,9 +4,7 @@ from homeassistant.core import HomeAssistant
 DOMAIN = "garo_entity_balance_meter"
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
