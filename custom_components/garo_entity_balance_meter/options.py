3dfd3d248b494390ba70a47c8f6a924c72d9a717
JanJoh <j2@mupp.net>
2026-03-24 15:39:22 +0100

    Update options.py

--- custom_components/garo_entity_balance_meter/options.py
+++ custom_components/garo_entity_balance_meter/options.py
@@ -1,12 +1,8 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import callback
-from .const import DOMAIN
+
 
 class GaroOptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
@@ -14,21 +10,21 @@ class GaroOptionsFlowHandler(config_entries.OptionsFlow):
         data = self.config_entry.data
         options = self.config_entry.options
 
-        def get(key, default):
+        def get_value(key, default):
             return options.get(key, data.get(key, default))
 
         return self.async_show_form(
             step_id="init",
-            data_schema=vol.Schema({
-                vol.Required("host", default=get("host", "")): str,
-                vol.Required("username", default=get("username", "")): str,
-                vol.Required("password", default=get("password", "")): str,
-                vol.Optional("scan_interval", default=get("scan_interval", 15)): int,
-                vol.Optional("ignore_tls_errors", default=get("ignore_tls_errors", True)): bool,
-            }),
+            data_schema=vol.Schema(
+                {
+                    vol.Required("host", default=get_value("host", "")): str,
+                    vol.Required("username", default=get_value("username", "")): str,
+                    vol.Required("password", default=get_value("password", "")): str,
+                    vol.Optional("scan_interval", default=get_value("scan_interval", 15)): int,
+                    vol.Optional(
+                        "ignore_tls_errors",
+                        default=get_value("ignore_tls_errors", True),
+                    ): bool,
+                }
+            ),
         )
-
-@callback
-def async_get_options_flow(config_entry):
-    return GaroOptionsFlowHandler(config_entry)
-
